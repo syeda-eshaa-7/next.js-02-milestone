cbdec804bb4e76d39053f0731cc62ad6a6f4f90e
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-21 19:04:01 +0000

    About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,12 +23,9 @@ const About = () => {
             specific language, but choosing the best tool for the job.
           </p>
           <p className='py-2 text-gray-600'>
-            I started web developement in 2013 managing multiple e-commerce
-            websites on CMS platforms such as WordPress, BigCommerce, and
-            Shopify. I have experience working directly with clients and taking
-            mock wireframes all the way to deployed applications. In my spare
-            time I run Code Commerce, a Youtube channel where I teach web
-            developement and various front-end technologies.
+            My journey in web development started with a passion for design and technology. 
+            I love transforming ideas into engaging interfaces that enhance user experiences.
+            I'm always eager to learn new skills and stay updated with the latest trends in the industry.
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
