64f7b32dbc38049964d853f45b226143d83df784
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-21 18:47:32 +0000

    HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,60 +1,59 @@
-import React from "react";
-import { AiOutlineMail } from "react-icons/ai";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import Typewriter from "typewriter-effect";
-const skilles: string[] = ["Frontend Developer", "UI/UX Designer"];
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
-function HeroSection() {
+const HeroSection = () => {
   return (
-    <div className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+    <div id='home' className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <p className="uppercase text-xl tracking-widest text-gray-600">
+          <p className='uppercase text-sm tracking-widest text-gray-600'>
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
-          <h1 className="py-4 text-gray-700 font-bold text-3xl">
-            {" "}
-            Hi, I&#39;m <span className="text-[#5651e5] text-3xl"> Syeda Esha</span>
+          <h1 className='py-4 text-gray-700'>
+            Hi, I&#39;m <span className='text-[#5651e5]'> Clint</span>
           </h1>
-          <h1 className="py-3 text-gray-700 text-2xl">A Front-End Web Developer</h1>
-          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
-            I'm a front-end web developer specializing (and occasinolly
-            designig) exceptional digital experiences, Currently I’m focused on
-            building responsive front-end web applications integrating back-end
-            technologies.
+          <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
+          <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
+            I’m focused on building responsive front-end web applications
+            integrating back-end technologies.
           </p>
-          <div className="flex it justify-between max-w-[330px] m-auto py-4">
-          <a
-                  href='https://www.linkedin.com/in/syeda-esha-0532692b4/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                    <FaLinkedinIn />
-                  </div>
-                </a>
-                <a
-                  href='https://github.com/syeda-eshaa-7'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                    <FaGithub />
-                  </div>
-                </a>
-
-          <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration">
-                  <AiOutlineMail />
-                </div>
-          <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration">
-                  <BsFillPersonLinesFill />
-                </div>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <a
+              href='https://www.linkedin.com/in/clint-briley-50056920a/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaLinkedinIn />
+              </div>
+            </a>
+            <a
+              href='https://github.com/fireclint'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaGithub />
+              </div>
+            </a>
+            <Link href='/#contact'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineMail />
+              </div>
+            </Link>
+            <Link href='/resume'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <BsFillPersonLinesFill />
+              </div>
+            </Link>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default HeroSection;
