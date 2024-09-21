f32f66617a5a965a5a3aa4598d63959e3e9523be
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-21 18:54:38 +0000

    page.tsx

--- src/app/property/page.tsx
+++ src/app/property/page.tsx
@@ -13,7 +13,7 @@ const property = () => {
           className='absolute z-1'
           layout='fill'
           objectFit='cover'
-          src={'/assets/projects/property.jpg'}
+          src={propertyImg}
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
