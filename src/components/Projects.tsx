bcdf5634b06ba2673e02f0cec54d3e688310dad3
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-22 14:30:32 +0500

    Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import propertyImg from '../public/assets/projects/property.jpg';
-import cryptoImg from '../public/assets/projects/crypto.jpg'
-import netflixImg from '../public/assets/projects/netflix.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
-import ProjectItem from './ProjectItem';
+
 
 const Projects = () => {
   return (
