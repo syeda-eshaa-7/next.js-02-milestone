57c23bf4b45eda541e7d99d3c91bafc077caa4f4
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-22 12:05:42 +0000

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 "use client"
+import Link from 'next/link';
+import Image from 'next/image';
 
 import React, { useState, useEffect } from 'react';
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
