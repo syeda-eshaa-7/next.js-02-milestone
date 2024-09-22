606599c14e524ed793a473ab8292d1293715e527
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-22 14:31:13 +0500

    Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,14 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import Html from '../public/assets/skills/html.png';
-import Css from '../public/assets/skills/css.png';
-import Javascript from '../public/assets/skills/javascript.png';
-import ReactImg from '../public/assets/skills/react.png';
-import Tailwind from '../public/assets/skills/tailwind.png';
-import Github from '../public/assets/skills/github1.png';
-import Firebase from '../public/assets/skills/firebase.png';
-import NextJS from '../public/assets/skills/nextjs.png'
-import AWS from '../public/assets/skills/aws.png';
+
 
 const Skills = () => {
   return (
