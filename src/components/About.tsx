ccf121cc8a80bb5f12ac9a0479ea0bf483e4a31d
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-22 13:17:51 +0000

    About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -34,7 +34,7 @@ const About = () => {
           </Link>
         </div>
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={AboutImg} className='rounded-xl' alt='/' />
+          <Image src={'assets/about.jpg'} className='rounded-xl' alt='/' />
         </div>
       </div>
     </div>
