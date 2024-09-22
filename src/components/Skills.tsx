bd09c98bc77b0b872a602e2d4d3bc3f23b64752d
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-22 13:22:26 +0000

    Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
 
-
-const Skills = () => {
+// Skills component with no props, so no need for prop typing
+const Skills: React.FC = () => {
   return (
     <div id='skills' className='w-full lg:h-screen p-2'>
       <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
@@ -11,83 +11,98 @@ const Skills = () => {
         </p>
         <h2 className='py-4'>What I Can Do</h2>
         <div className='grid grid-cols-2 lg:grid-cols-4 gap-8'>
+          {/* Skill Item for HTML */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={Html} width='64px' height='64px' alt='/' />
+                <Image src={'/assets/skills/html.png'} width={64} height={64} alt='HTML' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>HTML</h3>
               </div>
             </div>
           </div>
+
+          {/* Skill Item for CSS */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={Css} width='64px' height='64px' alt='/' />
+                <Image src={'/assets/skills/css.png'} width={64} height={64} alt='CSS' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>CSS</h3>
               </div>
             </div>
           </div>
+
+          {/* Skill Item for JavaScript */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={Javascript} width='64px' height='64px' alt='/' />
+                <Image src={'/assets/skills/javascript.png'} width={64} height={64} alt='JavaScript' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>JavaScript</h3>
               </div>
             </div>
           </div>
+
+          {/* Skill Item for React */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={ReactImg} width='64px' height='64px' alt='/' />
+                <Image src={'/assets/skills/react.png'} width={64} height={64} alt='React' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>React</h3>
               </div>
             </div>
           </div>
+
+          {/* Skill Item for Tailwind */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={Tailwind} width='64px' height='64px' alt='/' />
+                <Image src={'/assets/skills/tailwind.png'} width={64} height={64} alt='Tailwind' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>Tailwind</h3>
               </div>
             </div>
           </div>
+
+          {/* Skill Item for Firebase */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={Firebase} width='64px' height='64px' alt='/' />
+                <Image src={'/assets/skills/firebase.png'} width={64} height={64} alt='Firebase' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>Firebase</h3>
               </div>
             </div>
           </div>
+
+          {/* Skill Item for GitHub */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={Github} width='64px' height='64px' alt='/' />
+                <Image src={'/assets/skills/github1.png'} width={64} height={64} alt='GitHub' />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>Github</h3>
+                <h3>GitHub</h3>
               </div>
             </div>
           </div>
+
+          {/* Skill Item for Next.js */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={NextJS} width='64px' height='64px' alt='/' />
+                <Image src={'/assets/skills/nextjs.png'} width={64} height={64} alt='Next.js' />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>Next</h3>
+                <h3>Next.js</h3>
               </div>
             </div>
           </div>
