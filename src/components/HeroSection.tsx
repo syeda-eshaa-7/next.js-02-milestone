dae870e7e8bd4ad4c3132ab88bd94982112e2c3d
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-10-02 19:22:38 +0000

    HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <a
-              href='https://www.linkedin.com/in/clint-briley-50056920a/'
+              href='https://www.linkedin.com/in/syeda-esha-0532692b4/'
               target='_blank'
               rel='noreferrer'
             >
@@ -31,7 +31,7 @@ const HeroSection = () => {
               </div>
             </a>
             <a
-              href='https://github.com/fireclint'
+              href='https://github.com/syeda-eshaa-7'
               target='_blank'
               rel='noreferrer'
             >
