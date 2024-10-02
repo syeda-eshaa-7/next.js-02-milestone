0e6a294a921628818c4675043d5829d6013a482f
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-10-02 19:32:31 +0000

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -179,7 +179,7 @@ const Navbar = () => {
               </p>
               <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                 <a
-                  href='https://www.linkedin.com/in/clint-briley-50056920a/'
+                  href='https://www.linkedin.com/in/syeda-esha-0532692b4/'
                   target='_blank'
                   rel='noreferrer'
                 >
@@ -188,7 +188,7 @@ const Navbar = () => {
                   </div>
                 </a>
                 <a
-                  href='https://github.com/fireclint'
+                  href='https://github.com/syeda-eshaa-7'
                   target='_blank'
                   rel='noreferrer'
                 >
